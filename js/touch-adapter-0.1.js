b8480850e9067a641f5f64583f1f115d0ca4ff2c
sporritt <simon.porritt@gmail.com>
2013-11-08 12:32:43 +1100

    added support for smart click handling, and a test page for it

--- js/touch-adapter-0.1.js
+++ js/touch-adapter-0.1.js
@@ -18,14 +18,45 @@
 */
 ;(function() {
 
-	var isIOS = ((/iphone|ipad/gi).test(navigator.appVersion)),
-		isAndroid = false,
+	var isIOS = (function() { ((/iphone|ipad/gi).test(navigator.appVersion)) })(),
+		isTouchDevice = "ontouchstart" in document.documentElement,
 		click = "click", dblclick = "dblclick", mousedown = "mousedown", mouseup = "mouseup", mousemove = "mousemove",
-		touchstart = "touchstart", touchend = "touchend", touchmove = "touchmove",
-		contextmenu = "contextmenu", ta_is_down = "__touchAdaptorIsDown", ta_click_timeout = "__touchAdaptorClickTimeout",
+		touchstart = "touchstart", touchend = "touchend", touchmove = "touchmove", contextmenu = "contextmenu",
+		downEvent = isTouchDevice ? touchstart : mousedown,
+		upEvent = isTouchDevice ? touchend : mouseup,
+		moveEvent = isTouchDevice ? touchmove : mousemove,
+		ta_is_down = "__touchAdaptorIsDown", ta_click_timeout = "__touchAdaptorClickTimeout",
 		ta_context_menu_timeout = "__touchAdaptorContextMenuTimeout",
 		ta_down = "__touchAdapterDown", ta_up = "__touchAdapterUp", 
 		ta_context_down = "__touchAdapterContextDown", ta_context_up = "__touchAdapterContextUp",
+		_pageLocation = function(e) {
+			if (e.pageX && e.pageY)
+				return [e.pageX, e.pageY];
+			else {
+				var ts = _touches(e),
+					t = _getTouch(ts, 0);
+				// this is for iPad. may not fly for Android.
+				// NOTE this is a browser event here. you
+				// must first have passed the library event through the current event unwrap function.
+				return [t.pageX, t.pageY];
+			}        	
+		},
+		// 
+		// extracts the touch with the given index from the list of touches
+		//
+		_getTouch = function(touches, idx) {
+			return touches.item ? touches.item(idx) : touches[idx];
+		},
+		//
+		// gets the touches from the given event, if they exist. NOTE this is a browser event here. you
+		// must first have passed the library event through the current event unwrap function.
+		//
+		_touches = function(e) {	
+			return e.touches || [];
+		},
+		_touchCount = function(e) {
+			return _touches(e).length || 1;
+		},
 		//http://www.quirksmode.org/blog/archives/2005/10/_and_the_winner_1.html
 		addEvent = function( obj, type, fn ) {
 			if (obj.addEventListener)
@@ -61,12 +92,46 @@
 			wrapDown = params.wrapDown !== false,
 			wrapUp = params.wrapUp !== false,
 			wrapMove = params.wrapMove !== false,
-			_getTouchCount = function(e) {
-				var t = 1;
-				if (e.touches) {
-					t = e.touches.length;	  // what about android? and other mobile browsers?
-				}				
-				return t;
+			_smartClicks = params.smartClicks,			
+			_smartClickDown = function(e, obj) {
+				console.log("smart click down");				
+				obj.__tad = _pageLocation(_unwrap(e));
+				return true;
+			},
+			_smartClickUp = function(e, obj) {
+				console.log("smart click up");
+				obj.__tau = _pageLocation(_unwrap(e));
+				return true;
+			},
+			_smartClickClick = function(e, obj) {
+				if (obj.__tad && obj.__tau) {
+					return obj.__tad[0] == obj.__tau[0] && obj.__tad[1] == obj.__tau[1];
+				}
+				return true;
+			},
+			_smartClickHandlers = {
+				"mousedown":_smartClickDown,
+				"mouseup":_smartClickUp,
+				"touchstart":_smartClickDown,
+				"touchend":_smartClickUp,
+				"click":_smartClickClick
+			},
+			// wrap bind function to provide "smart" click functionality, which prevents click events if
+			// the mouse has moved between up and down.
+			__bind = function(obj, evt, fn) {
+				if (_smartClicks) {
+					var _fn = fn;
+					fn = function(e) {																
+						var cont = true;
+						if (_smartClickHandlers[evt])
+							cont = _smartClickHandlers[evt](e, obj);
+
+						if (cont) _fn.apply(this, arguments);
+					};
+				}
+				
+				_bind(obj, evt, fn);
+				
 			},
 			_addClickWrapper = function(obj, fn, touchCount, downId, upId, supportDoubleClick) {
 				var handler = {
@@ -77,9 +142,8 @@
 					timeout:null
 				};
 				var down = function(e) {						
-					var ee = _unwrap(e), self = this, tc = _getTouchCount(ee);					
-					if (tc == touchCount) {
-				
+					var ee = _unwrap(e), self = this, tc = _touchCount(ee);					
+					if (tc == touchCount) {				
 						handler.originalEvent = ee;	
 						handler.touches = tc;										
 						handler.down = true;							
@@ -89,7 +153,7 @@
 					}
 				};
 				fn[downId] = down;
-				_bind(obj, touchstart, down);	
+				__bind(obj, touchstart, down);	
 				var up = function(e) {										
 					var ee =  _unwrap(e);					
 					if (handler.down) {
@@ -110,11 +174,12 @@
 					window.clearTimeout(handler.timeout);						
 				};				
 				fn[upId] = up;	
-				_bind(obj, touchend, up);
+				__bind(obj, touchend, up);
 			};
+
 		
 		this.bind = function(obj, evt, fn) {
-			if (isIOS) {			
+			if (isTouchDevice) {			
 				if (evt === click && wrapClick) {
 					_addClickWrapper(obj, fn, 1, ta_down, ta_up);
 				}
@@ -125,19 +190,19 @@
 					_addClickWrapper(obj, fn, 2, ta_context_down, ta_context_up);
 				}
 				else if (evt === mousedown && wrapDown) {
-					_bind(obj, touchstart, fn);	
+					__bind(obj, touchstart, fn);	
 				}
 				else if (evt === mouseup && wrapUp) {
-					_bind(obj, touchend, fn);
+					__bind(obj, touchend, fn);
 				}			
 				else if (evt === mousemove && wrapMove) {
-					_bind(obj, touchmove, fn);
+					__bind(obj, touchmove, fn);
 				}
 				else
-					_bind(obj, evt, fn);
+					__bind(obj, evt, fn);
 			}
 			else 
-				_bind(obj, evt, fn);
+				__bind(obj, evt, fn);
 
 			return self;
 		};
